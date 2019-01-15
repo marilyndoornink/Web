595983973d5159517aacd0d7bb85754d66abbf98
marilyndoornink <45425525+marilyndoornink@users.noreply.github.com>
2019-01-15 11:45:42 +0100

    Add files via upload

---
+++ Website_Marilyn_Doornink_213/js/interacties.js
@@ -0,0 +1,36 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+//
+//function leesduuropenen() {
+//    document.querySelector('#leesduurmenu').style.display = 'block';
+//    console.log('test');
+//}
+//
+//document.querySelector('#leesduurknop').addEventListener('click', leesduuropenen);
+//
+////code voor kruisje
+//
+//
+//function leesduursluiten() {
+//    document.querySelector('#leesduurmenu').style.display = 'none';
+//    console.log('test');
+//}
+//
+//document.querySelector('#hier').addEventListener('click', leesduursluiten);
+//
+//
+
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Gelezen";
+}
+
+
+
+
+
+
+
